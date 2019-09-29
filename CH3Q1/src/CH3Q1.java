756cd34b577a889ac39f68f30244a3f14f7bf371
migasun <migasun@gmail.com>
2019-09-29 10:06:45 +0800

    Initial commit

---
+++ CH3Q1/src/CH3Q1.java
@@ -0,0 +1,11 @@
+public class CH3Q1 {
+    public static void main(String[] args) {
+        byte a='a';
+        short b=11;
+        int c=11;
+        long d=11;
+        float e=11;
+        double f=11;
+
+    }
+}
