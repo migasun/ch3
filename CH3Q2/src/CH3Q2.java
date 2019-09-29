47a10b712a1b9c25f24e6ffdc4ac873751da0972
migasun <migasun@gmail.com>
2019-09-29 10:16:48 +0800

    ch3 q1 che q2

---
+++ CH3Q2/src/CH3Q2.java
@@ -0,0 +1,19 @@
+public class CH3Q2 {
+    public static void main(String[] args) {
+        //請計算出 三筆資料 的平均
+        //例如 10 , 20 , 20 的平均 =  16.666…
+        double a = 10;
+        double b = 20;
+        double c = 20;
+
+        System.out.println((a + b + c) / 3);
+
+
+        int sum = 5;
+        System.out.println(sum++);//    請問輸出的結果為?
+        sum = 5;
+        System.out.println(++sum);//    請問輸出的結果為
+
+
+    }
+}
